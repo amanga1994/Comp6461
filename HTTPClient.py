1442f335a40dbf2a6d25d4b76446c5a1d031ab01
aman1512 <aman.ga151294@gmail.com>
2019-09-26 19:00:04 -0400

    post functionality added but not tested

--- HTTPClient.py
+++ HTTPClient.py
@@ -8,23 +8,44 @@ def uri_validator(x):
     try:
         result = urllib.urlparse(x)
         return all([result.scheme, result.netloc, result.path])
-    except:
+    except Exception as e:
+        print(e)
+        sys.exit()
         return False
 
 
-def http_client(host, string):
+def read_from_file(path):
+    out=""
+    try:
+        f = open(path, "r")
+        out = f.read()
+        return True, out
+    except Exception as e:
+        out = e
+        return False, out
+
+
+def write_to_file(file_path, output):
+    try:
+        f = open(file_path,"a+")
+        f.write(output)
+        print(f"Output write to file:{file_path}")
+        f.close()
+    except Exception as e:
+        print(e)
+        print("Please find below the output:\n"+output)
+
+
+def http_client(host, request):
     conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     output = ""
     try:
         conn.connect((host, 80))
         print("Type any thing then ENTER. Press Ctrl+C to terminate")
         print(ord(" "))
-        a = "{element:'fire'"
-        q = urllib.parse.quote("name=' aman'")
+        request = request.encode("utf-8")
+        conn.send(request)
         while True:
-            line = f"PUT /put?{q} HTTP/1.0\r\nAccept:application/json\r\n\r\n"
-            request = line.encode("utf-8")
-            conn.send(request)
             # MSG_WAITALL waits for full request or error
             response = conn.recv(len(request), socket.MSG_WAITALL)
             if len(response) > 0:
@@ -36,44 +57,122 @@ def http_client(host, string):
         conn.close()
 
 
-def implement_help(name):
-    if len(sys.argv)==3:
-        if sys.argv[2].lower() == "get":
+def implement_help():
+    if len(sys.argv) == 3 and args.help_for is not None :
+        if arg.help_for.lower() == "get":
             print("get help")
-        elif sys.argv[2].lower() == "post":
+        elif arg.help_for.lower() == "post":
             print("post help")
-        else:
-            print("invalid argument")
+    elif len(sys.argv) == 2:
+        print("program help")
     else:
-        print("help")
+        print("Invalid input")
 
 
-def implement_get():
-    parser.add_argument("-v", "--verbose", action="store_true")
-    parser.add_argument("-h", "--h", action="append", nargs="+")
-    parser.add_argument("URL")
+def implement_post():
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument("-v", "--v", action="store_true", help="Display additional information")
+    parser.add_argument("-h", "--h", action="append", nargs="+", help="Input headers for the request")
+    parser.add_argument("-o", "--o", help="Output data to file")
+    group.add_argument("-d", "--d", help="Input data for body from the console")
+    group.add_argument("-f", "--f", help="Input data for body from the file")
+    parser.add_argument("URL", help="URL for sending request")
     args = parser.parse_args()
-    URL = args.URL
-    if uri_validator(URL):
+    url = args.URL
+    if uri_validator(url):
         print("valid")
-        start = URL.find("/")
+        start = url.find("/")
         n = 3
         while start >= 0 and n > 1:
-            start = URL.find(URL, start + len("/"))
+            start = url.find(url, start + len("/"))
             n -= 1
             """if URL.find("?") >= 0:
                 path = URL.substring(start, URL.find())
             else:
                 path = URL.substring(start, len(URL))"""
-        path = URL.substring(start, len(URL))
-        start_path =  start
+        path = url.substring(start, len(url))
+        start_path = start
         path = urllib.parse.quote(path)
-        start = URL.find("/")
+        start = url.find("/")
         n = 2
         while start >= 0 and n > 1:
-            start = URL.find(URL, start + len("/"))
+            start = url.find(url, start + len("/"))
             n -= 1
-        host = URL.substring(start+1,start_path)
+        host = url.substring(start+1, start_path)
+
+        if args.h is None:
+            request = f"GET {path} HTTP/1.0\r\nAccept:application/json\r\n\r\n"
+        else:
+            request = f"GET {path} HTTP/1.0\r\n"
+            if "".join("".join(i) for i in args.h).lower().find("Accept") < 0:
+                request += "Accept:application/json\r\n"
+            for i in args.h:
+                request += "".join(i)+"\r\n"
+            request += "\r\n"
+        if args.d is not None and args.f is not None:
+            print("-d and -f both are not allowed in the same command")
+            sys.exit()
+        elif args.d is not None:
+            request += args.d+"\r\n\r\n"
+        else:
+            read_successful, output = read_from_file(args.f)
+            if read_successful:
+                request += output+"\r\n\r\n"
+            else:
+                print(output)
+                sys.exit()
+        reply = http_client(host, request)
+        final_output = ""
+        if args.v:
+            final_output = reply
+        else:
+            if str.find("200 OK"):
+                flag = 0
+                for i in reply.split("\r\n"):
+                    if len(i) == 0:
+                        flag = 1
+                    if flag == 1:
+                        final_output += i
+            else:
+                final_output = reply
+        if args.o is not None:
+            write_to_file(args.o, final_output)
+        else:
+            print(final_output)
+    else:
+        print("Please enter valid URL")
+
+
+def implement_get():
+    group = parser.add_mutually_exclusive_group()
+    parser.add_argument("-v", "--v", action="store_true", help="Display additional information")
+    parser.add_argument("-h", "--h", action="append", nargs="+", help="Input headers for the request")
+    parser.add_argument("-o", "--o", help="Output data to file")
+    parser.add_argument("URL", help="URL for sending request")
+    args=parser.parse_args()
+    url = args.URL
+    parser.add_argument("URL", nargs="?", help="URL for sending request")
+    url = args.URL
+    if uri_validator(url):
+        print("valid")
+        start = url.find("/")
+        n = 3
+        while start >= 0 and n > 1:
+            start = url.find(url, start + len("/"))
+            n -= 1
+            """if URL.find("?") >= 0:
+                path = URL.substring(start, URL.find())
+            else:
+                path = URL.substring(start, len(URL))"""
+        path = url.substring(start, len(url))
+        start_path = start
+        path = urllib.parse.quote(path)
+        start = url.find("/")
+        n = 2
+        while start >= 0 and n > 1:
+            start = url.find(url, start + len("/"))
+            n -= 1
+        host = url.substring(start+1, start_path)
 
         if args.h is None:
             request = f"GET {path} HTTP/1.0\r\nAccept:application/json\r\n\r\n"
@@ -85,7 +184,7 @@ def implement_get():
                 request += "".join(i)+"\r\n"
             request += "\r\n"
         reply = http_client(host, request)
-        final_output =""
+        final_output = ""
         if args.v:
             final_output = reply
         else:
@@ -96,18 +195,25 @@ def implement_get():
                         flag = 1
                     if flag == 1:
                         final_output += i
+            else:
+                final_output = reply
+        if args.o is not None:
+            write_to_file(args.output, final_output)
+        else:
+            print(final_output)
+    else:
+        print("Please enter valid URL")
 
 
 # Usage: python echoclient.py --host host --port port
-parser = argparse.ArgumentParser(add_help=False)
-parser.add_argument("method", help="HTTP method needed to implement", type=str, default=80)
-if parser.parse_args().method.lower() == "get":
+parser = argparse.ArgumentParser(add_help=False, prog = "httpc")
+parser.add_argument("method", help="HTTP method needed to implement", type=str, choices=["get", "post", "GET", "POST", "help", "HELP"])
+parser.add_argument("help_for", nargs="?", help="get help for",choices=["get", "post", "GET", "POST"])
+arg = parser.parse_args()
+if arg.method.lower() == "get":
     implement_get()
-elif parser.parse_args().method.lower() == "help":
+elif arg.method.lower() == "help":
     implement_help()
-
-
-
-parser.add_argument("URL", help="server host", default="httpbin.org")
-args = parser.parse_args()
+elif arg.method.lower() == "post":
+    implement_post()
 # http_client(args.host, args.port)
